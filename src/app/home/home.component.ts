c0783a6f32fc693d5d0b3c9823bcbf646948a913
hyl <hanyongle@zgzb.com>
2024-08-14 10:33:13 +0800

    --

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common'
+import { Component } from '@angular/core'
+import { FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  currentItem: any
+  title = 'click me'
+  imgurl = 'http://'
+  arr = [1, 3, 4]
 
+  a = 5
+  b = 2
+
+  sw = 'b'
+
+  titClick = () => {
+    console.log(666)
+    this.title = 'hhhha'
+  }
+
+  name = new FormControl('')
 }
